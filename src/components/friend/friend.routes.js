141686765eb12bc00570e69246285b98df6a606f
wh1t3ni9ht <wh1t3ni9ht@gmail.com>
2024-05-17 12:50:46 +0300

    create friend routes

--- src/components/friend/friend.routes.js
+++ src/components/friend/friend.routes.js
@@ -7,3 +7,17 @@
   5. GET /friend/accept/:userId
   6. DELETE /friend/:userId
 */
+
+import { Elysia, t } from "elysia";
+
+import friendController from "./friend.controller";
+
+const friends = new Elysia({ prefix: "/friend" })
+  .get("/list", friendController.list)
+  .get("/:userId", friendController.details)
+  .get("/search/:usernameOrEmail", friendController.search)
+  .get("/request/:userId", friendController.request)
+  .get("/accept/:userId", friendController.accept)
+  .delete("/:userId", friendController.remove);
+
+export default friends;
