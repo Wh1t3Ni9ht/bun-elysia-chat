1108aed70aaf3f9cb33688c12394207feaa2290c
wh1t3ni9ht <wh1t3ni9ht@gmail.com>
2024-05-17 12:49:05 +0300

    list & update preferences

--- src/components/preference/preference.controller.js
+++ src/components/preference/preference.controller.js
@@ -3,7 +3,7 @@
   1. list
   2. update
 */
-import { User } from "../user/user.model";
+import Preference from "./preference.model";
 import jwt from "jsonwebtoken";
 import dbg from "debug";
 
@@ -13,14 +13,22 @@ const list = async ({ cookie: { auth }, set }) => {
   try {
     const token = auth.value;
     const jwtUser = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(jwtUser.id, "-__v -password");
+    const preference = await Preference.findOne({ user: jwtUser.id });
+
+    if (!preference) {
+      set.status = 401;
+
+      const e = new Error("Unauthorized");
+      e.name = "UNAUTHORIZED";
+      throw e;
+    }
 
     set.status = 200;
 
     return {
       error: null,
       data: {
-        user,
+        preference,
       },
     };
   } catch (error) {
@@ -28,7 +36,7 @@ const list = async ({ cookie: { auth }, set }) => {
     debug("Message:", error.message);
     debug("Error:", error);
 
-    if (error.name === "JsonWebTokenError") {
+    if (error.name === "JsonWebTokenError" || error.name === "UNAUTHORIZED") {
       set.status = 401;
 
       return {
@@ -52,6 +60,67 @@ const list = async ({ cookie: { auth }, set }) => {
   }
 }
 
+const update = async ({ body, cookie: { auth }, set }) => {
+  try {
+    const { emailVisibility, onlineStatusVisibility, locationVisibility, bioVisibility, lastSeenVisibility, profilePictureVisibility, twoFactorAuthentication, readReceipts } = body;
+    const token = auth.value;
+    const jwtUser = jwt.verify(token, process.env.JWT_SECRET);
+    const preference = await Preference.findOneAndUpdate({ user: jwtUser.id }, {
+      emailVisibility,
+      onlineStatusVisibility,
+      locationVisibility,
+      bioVisibility,
+      lastSeenVisibility,
+      profilePictureVisibility,
+      twoFactorAuthentication,
+      readReceipts
+    }, { new: true });
+
+    if (!preference) {
+      set.status = 401;
+
+      const e = new Error("Unauthorized");
+      e.name = "UNAUTHORIZED";
+      throw e;
+    }
+
+    set.status = 200;
+
+    return {
+      error: null,
+      data: {
+        preference,
+      },
+    }
+  } catch (error) {
+    debug("Name:", error.name);
+    debug("Message:", error.message);
+    debug("Error:", error);
+
+    if (error.name === "JsonWebTokenError" || error.name === "UNAUTHORIZED") {
+      set.status = 401;
+
+      return {
+        data: null,
+        error: {
+          name: "UNAUTHORIZED",
+          message: "Unauthorized",
+        },
+      }
+    }
+
+    set.status = 500;
+
+    return {
+      data: null,
+      error: {
+        name: error.name,
+        message: error.message,
+      },
+    }
+  }
+}
 export default {
   list,
+  update,
 }
